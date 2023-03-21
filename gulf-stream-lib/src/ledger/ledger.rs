e4e856cae28de9d60a6f721bdc7dfde68fbae9c9
datben <benjamindumont@live.com>
2023-03-21 19:54:47 +0100

    add broadcast and port prms

--- gulf-stream-lib/src/ledger/ledger.rs
+++ gulf-stream-lib/src/ledger/ledger.rs
@@ -1,12 +1,15 @@
+use crate::pb::node_client::NodeClient;
 use crate::pb::node_server::Node;
 use crate::pb::{SendBlockRequest, SendBlockResponse};
 use crate::state::block::Block;
 use crate::state::blockchain::Blockchain;
 use tokio::sync::Mutex;
+use tonic::transport::Endpoint;
 use tonic::{Request, Response, Status};
 
 pub struct Ledger {
     pub state: Mutex<Blockchain>,
+    pub other_nodes: Mutex<Vec<Endpoint>>,
 }
 
 #[tonic::async_trait]
@@ -27,4 +30,32 @@ impl Node for Ledger {
 
         return Ok(Response::new(reply));
     }
+}
+
+#[tonic::async_trait]
+impl Broadcaster for Ledger {
+    async fn broadcast(&self, request: SendBlockRequest) -> Result<(), Vec<Endpoint>> {
+        let nodes = self.other_nodes.lock().await.to_owned();
+        let mut failed_request = vec![];
+        for endpoint in nodes.iter() {
+            if let Ok(mut client) = NodeClient::connect(endpoint.to_owned()).await {
+                if let Err(_) = client
+                    .send_block(tonic::Request::new(request.to_owned()))
+                    .await
+                {
+                    failed_request.push(endpoint.to_owned())
+                }
+            }
+        }
+        if failed_request.is_empty() {
+            Ok(())
+        } else {
+            Err(failed_request)
+        }
+    }
+}
+
+#[tonic::async_trait]
+trait Broadcaster {
+    async fn broadcast(&self, request: SendBlockRequest) -> Result<(), Vec<Endpoint>>;
 }
--- gulf-stream-node/src/server.rs
+++ gulf-stream-node/src/server.rs
@@ -4,11 +4,24 @@ use gulf_stream_lib::{
 use tokio::sync::Mutex;
 use tonic::transport::Server;
 
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[arg(short, long)]
+    port: u64,
+}
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let addr = "[::1]:50051".parse()?;
+    let args = Args::parse();
+
+    let addr = format!("[::1]:{}", args.port).parse()?;
+
     let ledger = Ledger {
         state: Mutex::new(Blockchain::default()),
+        other_nodes: Mutex::new(vec![]),
     };
 
     Server::builder()
