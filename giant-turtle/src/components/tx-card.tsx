4e956049fda58e36f7ad0ea2e8126a78db5c200e
datben <benjamindumont@live.com>
2023-04-30 15:24:02 +0200

    update png

--- giant-turtle/src/components/tx-card.tsx
+++ giant-turtle/src/components/tx-card.tsx
@@ -1,12 +1,21 @@
 import { Transaction } from "@giant-turtle/proto/pb_pb";
 import { base58 } from "@scure/base";
 
+import Image from "next/image";
+import mypic from "../assets/seashell.png";
+const SeaShell = () => {
+  return (
+    <Image src={mypic} alt="Picture of the author" width="20" height="20" />
+  );
+};
+
 export default function TransactionCard(tx: Transaction) {
   return (
     <>
       {" "}
       tx : {base58.encode(tx.getSignature_asU8())}, payer :{" "}
-      {base58.encode(tx.getPayer_asU8())}, gas : {tx.getGas()} Seashell
+      {base58.encode(tx.getPayer_asU8())}, gas : {tx.getGas()}{" "}
+      <SeaShell></SeaShell>
     </>
   );
 }
--- giant-turtle/src/pages/index.tsx
+++ giant-turtle/src/pages/index.tsx
@@ -2,17 +2,18 @@ import GetBalance from "@giant-turtle/components/get-balance";
 import TxHistory from "@giant-turtle/components/tx-history";
 
 import Image from "next/image";
-import mypic from "../assets/giant-turtle.jpg";
-const MyImage = () => {
+import mypic from "../assets/giant-turtle.png";
+const GiantTurtle = () => {
   return (
-    <Image src={mypic} alt="Picture of the author" width="100" height="100" />
+    <Image src={mypic} alt="Picture of the author" width="150" height="100" />
   );
 };
+
 export default function Home() {
   return (
     <>
       <h1>
-        <MyImage></MyImage> Giant Turtle, a gulf-stream explorer !
+        <GiantTurtle></GiantTurtle> Giant Turtle, a gulf-stream explorer !
       </h1>
       <div>
         <p>History : </p>
